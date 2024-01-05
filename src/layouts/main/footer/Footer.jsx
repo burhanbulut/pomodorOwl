a903bea4dbb698aaa2dbf42fab7e373cd4ec345e
burhanbulut <69114033+burhanbulut@users.noreply.github.com>
2024-01-05 04:23:00 +0300

    fix

--- src/layouts/main/footer/Footer.jsx
+++ src/layouts/main/footer/Footer.jsx
@@ -5,12 +5,12 @@ function Footer() {
     return (
         <div className=' bottom-0 h-16 w-full flex align-middle justify-around items-center bg-gradient-to-r from-cyan-500 to-blue-500'>
             <div>
-                <a className='m-2' href={'https://github.com/burhanbulut'} ><GitHubIcon  /></a>
-                <a className='m-2' href={'https://www.linkedin.com/in/burhanbulut/'} ><LinkedInIcon /></a>
+                <a className='m-2 text-white' href={'https://github.com/burhanbulut'} ><GitHubIcon  /></a>
+                <a className='m-2 text-white' href={'https://www.linkedin.com/in/burhanbulut/'} ><LinkedInIcon /></a>
 
             </div>
             <div>
-                <p className='text-xs text-gray-500'>Burhan Bulut © 2024</p>
+                <p className='text-xs text-blue-50'>Burhan Bulut © 2024</p>
             </div>
         </div>
     )
